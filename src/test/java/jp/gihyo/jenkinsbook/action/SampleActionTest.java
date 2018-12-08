7d8bf94a31420cf7da5fee7f66f0cc853d077a9c
角張正文 <jiaozhangzhengwen@kakubarimasafuminoMacBook-Air.local>
2018-12-08 13:26:31 +0900

    change

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -30,7 +30,7 @@ public class SampleActionTest {
 		boolean result = action.checkParameter(request);
 		verify(request);
 
-		assertEquals(false, result);
+		assertEquals(true, result);
 	}
 
 	@Test
